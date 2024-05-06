0ff4ac66d230459fa3847d3a839563df547af942
000xzhou <xzhou92@gmail.com>
2024-05-06 17:02:30 -0400

    setup

--- config.js
+++ config.js
@@ -1,11 +1,12 @@
 /** Common config for bookstore. */
+require("dotenv").config();
 
 let DB_URI = process.env.DB_URI;
 
 if (process.env.NODE_ENV === "test") {
-  DB_URI = `${DB_URI}/books-test`;
+  DB_URI = `${DB_URI}/bookstore_test`;
 } else {
-  DB_URI = process.env.DATABASE_URL || `${DB_URI}/books`;
+  DB_URI = process.env.DATABASE_URL || `${DB_URI}/bookstore`;
 }
 
 module.exports = { DB_URI };
--- data.sql
+++ data.sql
@@ -1,3 +1,5 @@
+\c bookstore
+
 CREATE TABLE books (
   isbn TEXT PRIMARY KEY,
   amazon_url TEXT,
@@ -8,3 +10,5 @@ CREATE TABLE books (
   title TEXT, 
   year INTEGER
 );
+INSERT INTO books (isbn, amazon_url, author, language, pages, publisher, title, year)
+VALUES ('1234567890', 'https://www.amazon.com/book', 'John Doe', 'English', 200, 'Publisher', 'Sample Book', 2024);
